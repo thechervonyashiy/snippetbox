7d32ce614e272a02d26eb100753ffa15ba90c341
Ivan Chervonyashiy <thechervonyashiy@gmail.com>
2024-12-23 16:28:35 +0300

    1

---
+++ cmd/web/handlers/handlers.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	if err = ts.Execute(w, nil); err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+	w.Write([]byte("Привет из Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Отображение определенной заметки с ID %d...", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Метод не дозволен", 405)
+		return
+	}
+	w.Write([]byte("Создание новой заметки"))
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Запуск сервера на http://127.0.0.1:4000")
+	if err := http.ListenAndServe(":4000", mux); err != nil {
+		log.Fatal(err)
+	}
 }
